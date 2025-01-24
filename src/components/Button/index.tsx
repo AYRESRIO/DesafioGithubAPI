45c7e90730ae4a2cac440a044b1a26199bbedb4a
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2025-01-23 22:18:15 -0300

    route Home and component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function Button() {
+  return <div className="dga-button">Começar</div>;
+}
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,26 @@
+import Button from "../../components/Button";
 import Header from "../../components/Header";
+import "./styles.css";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="dga-container">
+          <section id="dga-home-section">
+            <div className="dga-title-home">
+              <h1>Desafio Github API</h1>
+            </div>
+            <div className="dga-escola">
+              <h3>DevSuperior - Escola de programação</h3>
+            </div>
+          </section>
+        </div>
+        <div>
+          <Button />
+        </div>
+      </main>
+    </>
+  );
 }
